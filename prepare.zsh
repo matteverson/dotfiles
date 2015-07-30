2683856b4b153212b0850a9877a119239fd24132
Matt Everson <meverson@tenable.com>
2015-07-30 14:31:31 -0700

    Add .vimrc

---
+++ prepare.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+#git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#chsh -s /bin/zsh
+setopt EXTENDED_GLOB
+
+ln -s "${ZDOTDIR:-$HOME}"/git/dotfiles/.gitconfig "${ZDOTDIR:-$HOME}/.gitconfig"
+
+for rcfile in "${ZDOTDIR:-$HOME}"/git/dotfiles/prezto/.*; do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
+done
+
+for rcfile in "${ZDOTDIR:-$HOME}"/git/dotfiles/tmux/.*; do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
+done
+
+for rcfile in "${ZDOTDIR:-$HOME}"/git/dotfiles/vim/.*; do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
+done
+
+#curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+
